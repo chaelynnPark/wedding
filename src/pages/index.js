4aa4fe22ab64456de215f9a1c0a9a438784d2f9c
chaelynn.park <chaelynn.park@seculayer.com>
2026-05-28 17:19:45 +0900

    화면 변경

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,11 +9,17 @@ import Title from "../components/title";
 import "../styles/index.css";
 
 import GroovePaper from "../assets/GroovePaper.png";
+import GalleryPhoto6 from "../assets/gallery_photo_6.jpg";
 import Location from "../components/location";
 import CongratulatoryMoney from "../components/congratulatoryMoney";
 import Share from "../components/share";
 import Quote from "../components/quote";
 import Song from "../assets/song.mp3";
+import {
+  GROOM_NAME,
+  BRIDE_NAME,
+  WEDDING_DATE,
+} from "../../config.js";
 
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -25,6 +31,40 @@ const Wrapper = styled.div`
   background: #efebe9;
   background-image: url(${GroovePaper});
   width: 100%;
+`;
+
+const Splash = styled.section`
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  background-image: url(${GalleryPhoto6});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+  padding-bottom: 12vh;
+`;
+
+const SplashNames = styled.p`
+  font-family: "mom_to_daughter";
+  color: #fff;
+  font-size: 2.75rem;
+  margin: 0 0 12px 0;
+  text-align: center;
+  text-shadow: 0 2px 12px rgba(0, 0, 0, 0.45);
+  letter-spacing: 0.05em;
+`;
+
+const SplashDate = styled.p`
+  font-family: "mom_to_daughter";
+  color: #fff;
+  font-size: 1.25rem;
+  margin: 0;
+  text-align: center;
+  text-shadow: 0 2px 10px rgba(0, 0, 0, 0.45);
 `;
 
 const IndexPage = () => {
@@ -49,6 +89,12 @@ const IndexPage = () => {
       <audio autoPlay loop>
         <source src={Song} />
       </audio>
+      <Splash>
+        <SplashNames>
+          {GROOM_NAME} &#38; {BRIDE_NAME}
+        </SplashNames>
+        <SplashDate>{WEDDING_DATE}</SplashDate>
+      </Splash>
       <Title />
       <Greeting />
       <Gallery />
